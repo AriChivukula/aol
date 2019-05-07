76c3bc5e45fe7f61db9834c382419b5bf2d02074
Ari Chivukula <ari@chivuku.la>
2019-05-07 14:40:23 -0700

    Create terraform.sh

---
+++ terraform.sh
@@ -0,0 +1,7 @@
+set -e
+
+wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+unzip terraform_0.11.13_linux_amd64.zip
+rm terraform_0.11.13_linux_amd64.zip
+./terraform init -backend-config="bucket=aol" -backend-config="key=terraform.tfstate" .
+./terraform plan .
