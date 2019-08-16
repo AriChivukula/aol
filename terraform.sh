60676ba479f8a6fca7f69b0fbd8791fd0ddf7d92
Ari Chivukula <ari@chivuku.la>
2019-08-15 19:28:20 -0700

    Update terraform.sh

--- terraform.sh
+++ terraform.sh
@@ -4,4 +4,3 @@ wget https://releases.hashicorp.com/terraform/0.12.6/terraform_0.12.6_linux_amd6
 unzip terraform_0.12.6_linux_amd64.zip
 rm terraform_0.12.6_linux_amd64.zip
 ./terraform init -backend-config="bucket=arionline" -backend-config="key=terraform.tfstate" .
-./terraform plan .
